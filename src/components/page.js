dbff3378032076b83bae2701dc83160fe520e5e2
Jaime Ramírez <jaime.ramirez@roiback.com>
2016-07-07 13:25:25 +0200

    Adjusted eslint rules

--- src/components/page.js
+++ src/components/page.js
@@ -1,32 +1,13 @@
-/*var isNode = typeof module !== 'undefined' && module.exports
-  , React = isNode ? require('react') : window.React
-  , ReactDOM = isNode ? require('react') : window.ReactDOM;
-
-var HelloMessage = React.createClass({
-  handleClick: function () {
-    alert('You clicked!');
-  },
-
-  render: function() {
-    return <div onClick={this.handleClick}>Hello {this.props.name}</div>
-  }
-})
-
-if (isNode) {
-  exports.HelloMessage = HelloMessage;
-} else {
-  ReactDOM.render(<HelloMessage name="John" />, document.getElementById('react-root'))
-}*/
-
 import React, { Component } from 'react';
 
 export default class Page extends Component {
 
-  handleClick () {
+  handleClick() {
     alert('You clicked!');
   }
 
   render() {
-    return <div onClick={this.handleClick}>Hello</div>
+    return <div onClick={this.handleClick}>Hello</div>;
   }
+
 }
