6299e10b6ef8b2f31e1fc591a21a94cc7b7c5fcf
Jaime Ramírez <jaime.ramirez@roiback.com>
2016-07-07 13:21:33 +0200

    Basic served side rendering achieved

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
+const path = require('path');
+require('babel-register');
 const express = require('express');
-const jade = require('jade');
-const reactDOM = require('react-dom/server');
+const React = require('react');
+const ReactDOM = require('react-dom/server');
+const pageComponent = require('./components/page').default;
 const app = express();
+const createPage = React.createFactory(pageComponent);
 
-app.engine('jade', jade.__express);
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use('/', express.static('public'));
+app.get('/', (req, res) => res.render('index', {
+  react: ReactDOM.renderToString(createPage())
+}));
 
 app.listen(process.env.PORT || 3000);
